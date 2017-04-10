1ee5f2b694620849901437d6ca40d2bf2ff413a5
nadinesk <nadine.khattak@gmail.com>
2017-04-10 14:40:14 -0400

    update cli

--- lib/now_playing/cli1.rb
+++ lib/now_playing/cli1.rb
@@ -29,27 +29,7 @@ class NowPlaying::CLI
 
   def start
     list
-    input = nil
-    while input != "exit"
-      puts ""
-      puts "What movie would you more information on, by name or number?"
-      puts ""
-      puts "Enter list to see the movies again."
-      puts "Enter exit to end the program."
-      puts ""
-      input = gets.strip
-      if input == "list"
-        list
-      elsif input.to_i == 0
-        if movie = NowPlaying::Movie.find_by_name(input)
-          print_movie(movie)
-        end
-      elsif input.to_i > 0
-        if movie = NowPlaying::Movie.find(input.to_i)
-          print_movie(movie)
-        end
-      end
-    end
+
     puts "Goodbye!!!"
-  end
+  
 end
